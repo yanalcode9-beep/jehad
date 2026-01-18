1bdbd9fac3035e0550610a1107ba7d7edfa3d1b9
yanalcode9-beep <yanalcode9@gmail.com>
2026-01-18 22:41:36 +0200

    Update Page.component.js

--- src/components/Page.component.js
+++ src/components/Page.component.js
@@ -1,4 +1,4 @@
-﻿import { Component } from '../core/Component.js';
+import { Component } from '../core/Component.js';
 import { Header } from './Header.component.js';
 import { About } from './About.component.js';
 import { Video } from './Video.component.js';
@@ -9,46 +9,36 @@ import { Experience } from './Experience.component.js';
 import { Academic } from './Academic.component.js';
 import { Connect } from './Connect.component.js';
 import { Footer } from './Footer.component.js';
-import { getStyles } from '../styles/main.styles.js';
-import { getClientScript } from '../scripts/client.script.js';
 
 export class Page extends Component {
     render() {
-        const lang = this.props.lang || 'en';
-        const isRTL = lang === 'ar';
-        
-        return `<!DOCTYPE html>
-<html lang="${lang}" dir="${isRTL ? 'rtl' : 'ltr'}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="${this.t('header.title')}">
-    <meta name="author" content="Yanal">
-    <title>${this.t('header.name')}</title>
-    <style>${getStyles(isRTL)}</style>
-</head>
-<body>
-    <button id="languageToggle" class="language-toggle" aria-label="Switch Language">
-        <span id="languageText">${this.t('language.switch')}</span>
-    </button>
-    <div id="particles"></div>
-    <div class="container">
-        ${new Header(this.props)}
-        <div class="content">
-            ${new About(this.props)}
-            ${new Video(this.props)}
-            ${new Achievements(this.props)}
-            ${new Contributions(this.props)}
-            ${new Focus(this.props)}
-            ${new Experience(this.props)}
-            ${new Academic(this.props)}
-            ${new Connect(this.props)}
-        </div>
-        ${new Footer(this.props)}
-    </div>
-<script>window.currentLang = '${lang}';</script>
-    <script src="/js/client.js"></script>
-</body>
-</html>`;
+        return `
+            <button 
+                id="languageToggle" 
+                class="language-toggle" 
+                aria-label="Switch Language"
+            >
+                ${this.t('language.switch')}
+            </button>
+
+            <div id="particles"></div>
+
+            <div class="container">
+                ${new Header(this.props).render()}
+
+                <div class="content">
+                    ${new About(this.props).render()}
+                    ${new Video(this.props).render()}
+                    ${new Achievements(this.props).render()}
+                    ${new Contributions(this.props).render()}
+                    ${new Focus(this.props).render()}
+                    ${new Experience(this.props).render()}
+                    ${new Academic(this.props).render()}
+                    ${new Connect(this.props).render()}
+                </div>
+
+                ${new Footer(this.props).render()}
+            </div>
+        `;
     }
 }
